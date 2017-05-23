0869b51f83b114aeaa494afb04fa1229de243299
Will Palmer <will@steelhive.com>
2017-05-23 11:31:29 -0400

    updating userdata, bootstrap scripts

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -55,12 +55,23 @@ function provision-node () {
 
     # if [ "$ROLE" == 'master' ]; ... may start some things and not others
     docker run -d \
+        -p 8300:8300 \
+        -p 8301:8301 \
+        -p 8301:8301/udp \
+        -p 8302:8302 \
+        -p 8302:8302/udp \
+        -p 8400:8400 \
+        -p 8500:8500 \
+        -p 8600:8600 \
         -e ROLE=$ROLE \
         -e HOST_IP=$HOST_IP \
-        -e JOIN_IPS=$JOIN_IPS \
+        -e JOIN_IPS="$JOIN_IPS" \
         $L5S_CORE_SVC
 
     docker run -d \
+        -p 4646:4646 \
+        -p 4647:4647 \
+        -p 4648:4648 \
         -e ROLE=$ROLE \
         -e HOST_IP=$HOST_IP \
         $L5S_CORE_EXEC
