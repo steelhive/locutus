42fbffbde7e3049f9d6e064d60133a95c9be7ece
Will Palmer <will@steelhive.com>
2017-05-26 16:16:03 -0400

    fixing policy docs i hope

--- infrastructure/modules/asg/main.tf
+++ infrastructure/modules/asg/main.tf
@@ -1,8 +1,27 @@
 
 resource "aws_iam_role" "role" {
     name                        = "${var.name}-read-only-role"
-    path                        = "/"
     assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "policy" {
+    name                        = "${var.name}-read-only-policy"
+    role                        = "${aws_iam_role.role.id}"
+    policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
